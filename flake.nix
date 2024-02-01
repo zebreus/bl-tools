9b2e87df2678c69621e12e88fd2cf3e77ca194d4
Zebreus <lennarteichhorn@googlemail.com>
2024-02-01 18:17:04 +0100

    Copy devshell from coreboot and add rust

--- flake.nix
+++ flake.nix
@@ -1,11 +1,66 @@
 {
-  description = "A very basic flake";
+  description = "A lot of utilities for working with the BL808 SoC";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=refs/heads/master";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system};
+      rec {
+        name = "bl808-tools";
+        devShells.default = mkShellNoCC
+          {
+            ncurses = ncurses;
+            buildInputs = [
+              gnat13 # gcc with ada
+              ncurses # make menuconfig
+              m4
+              flex
+              bison # Generate flashmap descriptor parser
+              go
+              dtc
+              nss
+              bc
+
+              # We need scan-build and ccc-analyzer, but only the version from clang-analyzer seems to work
+              # https://github.com/NixOS/nixpkgs/issues/151367
+
+              clang-analyzer
+              clang-tools_16
+              libxcrypt # For compiling clang before version 17
+              ccache
+
+              nil
+              zlib
+              openssl
+              #acpica-tools # iasl
+              pkg-config
+              qemu # test the image
+              gdb
+              python3
+
+              (fenix.packages.${system}.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              fenix.packages.${system}.rust-analyzer
+            ];
+            shellHook = ''
+              # TODO remove?
+              NIX_LDFLAGS="$NIX_LDFLAGS -lncurses"
+            '';
+          };
+
+      }
+    );
 }
